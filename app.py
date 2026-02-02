bc0de9b8be3a9ded796d9d9bbc9883969a3760f6
baslani <baslani@gmu.edu>
2026-02-02 14:20:21 -0500

    Updated the benefit part

--- app.py
+++ app.py
@@ -163,7 +163,9 @@ calls_per_year = 2000
 # avoided_cost_per_call = st.sidebar.number_input("Avoided officer response cost ($/call)", value=50.0)
 avoided_cost_per_call = 50.0
 
-ben_labor = st.sidebar.number_input("Labor Savings ($/yr)", value=0.0)
+ben_labor = st.sidebar.number_input("Labor Savings (count/yr)", value=0, step=1, min_value=0)
+avoided_labor = 50.0
+ben_labor_val = ben_labor * avoided_labor
 
 # safety_events_per_year = st.sidebar.number_input("Safety Improvements (count/yr)", value=0, step=1, min_value=0)
 # ben_safety = float(safety_events_per_year)  # numbers only
@@ -174,7 +176,7 @@ ben_dispatch = float(dispatch_units_reduced_per_year)  # numbers only
 response_time_savings = minutes_saved * value_per_min * calls_per_year
 cost_avoidance = avoided_cost_per_call * calls_per_year
 
-annual_benefits = response_time_savings + cost_avoidance + ben_labor +  ben_dispatch
+annual_benefits = response_time_savings + cost_avoidance + ben_labor_val +  ben_dispatch
 
 # ------------------------------------------------------------
 # ANALYSIS SETTINGS
